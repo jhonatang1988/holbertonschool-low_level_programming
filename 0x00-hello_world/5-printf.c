fd6d384e00e39a7ecc3f0efebb45ad51b92c4057
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-06-19 19:15:01 +0000

    solve task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - prints with printf
+* Return: always 0
+*/
+int main(void)
+{
+	char str1[55];
+
+	strcpy(str1, "with proper grammar, but the outcome is a piece of art,");
+	printf(str1);
+
+	return(0);
+}
