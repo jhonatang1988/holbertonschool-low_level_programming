53c997f8c1cc24d03aff5c8da0c6298b4b6500e8
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-06-20 21:19:30 +0000

    task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - from 00 to 99
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a1to9;
+	int b1to9;
+
+	for (a1to9 = 0; a1to9 <= 9; a1to9++)
+	{
+		for (b1to9 = 0; b1to9 <= 9; b1to9++)
+		{
+			putchar(a1to9 + '0');
+			putchar(b1to9 + '0');
+			if (a1to9 != 9 || b1to9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
