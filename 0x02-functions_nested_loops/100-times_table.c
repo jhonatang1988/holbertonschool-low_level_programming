315901531f021f1b785d1d2d648d197414bb590b
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-06-25 22:49:53 +0000

    solved spaces

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,41 +19,30 @@ void print_times_table(int n)
 			{
 				result = y * x;
 				if (result > 99 && result < 1000)
-				{
-					_putchar('0' + result / 100);
+				{ _putchar('0' + result / 100);
 					_putchar('0' + ((result / 10) % 10));
 					_putchar('0' + result % 10);
 				} else
 				{
 					if (result > 9 && result < 100)
-					{
-						_putchar(' ');
+					{ _putchar(' ');
 						_putchar('0' + result / 10);
 						_putchar('0' + result % 10);
 					} else
 					{
 						if (y == 0)
-						{
-							_putchar('0' + result);
-						} else
-						{
-						_putchar(' ');
+						{ _putchar('0' + result); }
+						else
+						{ _putchar(' ');
 						_putchar(' ');
 						_putchar('0' +  result);
-						}
-					}
-				}
+						} } }
 				if (y != n)
-				{
-					_putchar(',');
+				{ _putchar(',');
 					_putchar(' ');
-				}
-			}
+				} }
 			_putchar('\n');
-			spaces();
-		}
-	}
-}
+		} } }
 void spaces(void)
 {
 	_putchar(' ');
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,21 +6,22 @@
  */
 int main(void)
 {
-	long double fib1 = 0;
-	long double fib2 = 1;
+	long double fib1 = 1;
+	long double fib2 = 2;
 	long double fib3 = 0;
 	int count = 1;
 
+	printf("%.Lf, ", fib1);
 	printf("%.Lf, ", fib2);
 
-	while (count <= 49)
+	while (count <= 48)
 	{
 		fib3 = fib1 + fib2;
 		printf("%.Lf", fib3);
 		fib1 = fib2;
 		fib2 = fib3;
 		count++;
-		if (count != 50)
+		if (count != 49)
 			printf(", ");
 	}
 	printf("\n");
