8d6f2649305b4f35ca4649df3d28f65364a1858c
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-06-27 22:20:24 +0000

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,21 +7,19 @@
  */
 int main(void)
 {
-	long int number = 189;
+	long int number = 612852475143;
 	int i = 1;
 
 	while (i <= number)
 	{
 		if (number % i == 0)
 		{
-			if ( number == i){
+			if (number == i)
 				printf("%ld\n", number);
-				return (0);
-			}
 			number = number / i;
 			i = 1;
 		}
 		i++;
 	}
-	return(0);
+	return (0);
 }
