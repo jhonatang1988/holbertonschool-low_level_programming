00e3387f45de2622bf361636de13c51f03028f3d
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-02 16:25:36 +0000

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies a string using pointers
+ * @dest: destinations pointer
+ * @src: source pointer
+ *
+ * Return: nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int increment = 0;
+
+	while (*(src + increment) != '\0')
+	{
+		*(dest + increment) = *(src + increment);
+		increment++;
+	}
+
+	return (&*dest);
+}
