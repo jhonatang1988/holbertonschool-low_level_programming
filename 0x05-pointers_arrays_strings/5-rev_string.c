a300b91b84d2fb2daf175ab6dbfeb0bdcd7d2b43
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-02 14:39:31 +0000

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,28 @@
 #include "holberton.h"
 /**
  * rev_string - prints a string followed by a new line
- * @*s: pointer to string to revert back
+ * @s: pointer to string to revert back
  *
  * Return: nothing
  */
 void rev_string(char *s)
 {
 	int increment = 0;
-	int increment
+	int increment2 = 0;
+	int increment3 = 0;
+	char tmpstr[10];
 
 	while (*(s + increment) != '\0')
 	{
+		tmpstr[increment2] = (*(s + increment2));
 		increment++;
+		increment2++;
 	}
 
-	for (; increment >= 0; increment--)
+	for (; increment > 0; --increment)
 	{
-		_putchar(*(s + increment));
+		s[increment3] = tmpstr[increment - 1];
+		increment3++;
 	}
 
-	_putchar('\n');
 }
