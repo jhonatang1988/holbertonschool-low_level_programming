f357288eea439ccbced823625204ed3f6ce907c0
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-04 21:11:29 +0000

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalize an string
+ * @str: string
+ *
+ * Return: nothing
+ */
+char *cap_string(char *str)
+{
+	int counter = 0;
+	char wordsep[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '-', '\t', '\n'};
+	int wordsepcounter;
+
+	while (str[counter] != '\0')
+	{
+		wordsepcounter = 0;
+
+		while (wordsepcounter < 14)
+		{
+			if (str[counter] == wordsep[wordsepcounter])
+			{
+				if (str[counter + 1] > 96 && str[counter + 1] < 123)
+				{
+					str[counter + 1] = str[counter + 1] - 32;
+				}
+			}
+			wordsepcounter++;
+		}
+		counter++;
+	}
+
+	return (&(*str));
+}
