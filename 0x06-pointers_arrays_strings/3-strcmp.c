cab02bb597fca947f287b02fece58575126bcd70
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-04 19:02:42 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare to strings exact same as srtcmp
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: difference in ascii values
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int counter;
+	int diff;
+
+	for (counter = 0; s1[counter] != '\0' && s2[counter] != '\0'; counter++)
+	{
+		if (s1[counter] == s2[counter])
+			counter++;
+		else
+			break;
+	}
+
+	diff = s1[counter] - s2[counter];
+
+	return (diff);
+}
