e4510282aef2c99746328fad80614c257bc1ba01
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-16 20:10:42 +0000

    3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * alloc_grid - pointer to a 2d array of integers
+ *
+ * @width: width of 2d array
+ * @height: heigth of 2d array
+ *
+ * Return: pointer to the 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+
+	int i;
+	int j;
+	int** matrix = NULL;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	matrix = (int **)malloc(sizeof(int *)*height);
+
+	for (i = 0; i < height; i++)
+	{
+		matrix[i] = (int *)malloc(sizeof(int) * width);
+	}
+
+	for(i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+
+	return (matrix);
+}
