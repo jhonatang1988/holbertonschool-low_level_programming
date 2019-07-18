624ae5141b5a9d3e5348fcfc1abb91384227e916
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-18 21:27:54 +0000

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,26 +11,39 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newAddress = NULL;
 	unsigned int i;
 	char *castedNewAddress = NULL;
 	char *castedPtr = NULL;
 
-	if (old_size == new_size)
-		return (ptr);
-
-	newAddress = malloc(old_size + new_size);
-
-	if(newAddress == NULL)
-		return (NULL);
-
-	castedNewAddress = (char *) newAddress;
-	castedPtr = (char *) ptr;
-
-	for (i = 0; i < old_size ;i++)
+	if (ptr == NULL)
 	{
-		castedNewAddress[i] = castedPtr[i];
+		castedNewAddress = malloc(new_size);
+		if (castedNewAddress == NULL)
+			return (NULL);
+		else
+			return castedNewAddress;
+	}
+	else
+	{
+
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+
+		if (old_size == new_size)
+			return (ptr);
+
+		castedNewAddress = malloc(old_size + new_size);
+
+		castedPtr = (char *) ptr;
+
+		for (i = 0; i < old_size ;i++)
+		{
+			castedNewAddress[i] = castedPtr[i];
+		}
 	}
 
-	return (newAddress);
+	return(castedNewAddress);
 }
