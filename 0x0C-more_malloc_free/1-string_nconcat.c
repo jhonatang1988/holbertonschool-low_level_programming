e705621be0ddac246b240aa3269ad583d8521c49
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-18 19:11:02 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,10 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = lenghts2;
 
 	newStringAddress = malloc(lenghts1 + n + 1);
+
 	if (newStringAddress == NULL)
 		return (NULL);
 
-	for (i = 0; i < lenghts1; i++)
+	for (i = 0; i < lenghts1 - 1; i++)
 	{
 		newStringAddress[i] = s1[i];
 	}
@@ -62,5 +63,5 @@ unsigned int strlenght(char *s1)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	return (i);
+	return (i + 1);
 }
