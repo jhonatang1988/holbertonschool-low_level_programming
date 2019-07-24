e26676cba704528b602d900dcdab96b1d943f11b
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-24 22:01:11 +0000

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+/**
+ * op_add - sum of two integers
+ *
+ * @a: int one
+ * @b: int second
+ *
+ * Return: sum of integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract two integers
+ *
+ * @a: int one
+ * @b: int second
+ *
+ * Return: substraction of integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ *
+ * @a: int one
+ * @b: int second
+ *
+ * Return: multiplication of integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ *
+ * @a: int one
+ * @b: int second
+ *
+ * Return: division of integers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - module of two integers
+ *
+ * @a: int one
+ * @b: int second
+ *
+ * Return: module of integers
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
