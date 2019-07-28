2e476d26ec64e6fc29b0006b67e670fca68a2252
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-07-28 18:51:12 +0000

    2 y 3

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_strings - print string followed by a new line
+ *
+ * @separator: string to print between numbers
+ * @n: number of parameters
+ *
+ * Return: numbers
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+	unsigned int size;
+	char *string = NULL;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+
+		string= va_arg(strings, char*);
+		if (string == NULL)
+			printf("(nil)");
+		for (size = 0; string[size] != '\0'; size++)
+		{
+			printf("%c", string[size]);
+		}
+		putchar('\0');
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+		else
+			printf("\n");
+	}
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_all - prints anything
+ *
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	int j = 0;
+
+	va_list listToPrint;
+
+	va_start(listToPrint, format);
+	while(format[i])
+	{
+		i++;
+	}
+
+	while (format[j] != '\0')
+	{
+		switch(format[j])
+		{
+		case 'c' :
+			printf("%c", va_arg(listToPrint, int));
+			break;
+		case 'i' :
+			printf("%d", va_arg(listToPrint, int));
+			break;
+		case 's':
+			printf("%s", va_arg(listToPrint, char*));
+			break;
+		case 'f':
+			printf("%f", va_arg(listToPrint, double));
+		default:
+			;
+		}
+		j++;
+		if (j != i)
+		{
+			if(format[j] == 'c'
+			   || format[j] == 's'
+			   || format[j] == 'i'
+			   || format[j] == 'f')
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
