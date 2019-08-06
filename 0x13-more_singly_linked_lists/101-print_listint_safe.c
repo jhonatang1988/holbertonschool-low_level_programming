eab4ff557713b679fa9cc3e69136233a58db2189
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-06 20:52:16 +0000

    12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a linked list in safe mode
+ *
+ * @head: pointer to the head
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *temp = NULL;
+	const listint_t *temp2 = NULL;
+	const listint_t *flag = NULL;
+	const listint_t *temp3 = NULL;
+	int count = 0;
+	int len = 0;
+
+	temp = head;
+	temp2 = head->next->next;
+	temp3 = head;
+
+	while (count != 2)
+	{
+		if (flag)
+		{
+			if (flag == temp3)
+				count++;
+			if (count == 2)
+			{
+				printf("-> ");
+				len--;
+			}
+			printf("[%p] %d\n", (void *)temp3,  temp3->n);
+			temp3 = temp3->next;
+			len++;
+			if (!temp3)
+				return (len);
+		}
+		else
+		{
+			if (temp2->next == temp)
+			{
+				flag = temp2;
+			}
+			else
+			{
+				if (temp->next)
+					temp = temp->next;
+				else
+					flag = temp;
+				if (temp2->next->next)
+					temp2 = temp2->next->next;
+			}
+		}
+	}
+	return (len);
+}
