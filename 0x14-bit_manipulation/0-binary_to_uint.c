c0c76428f28f2c7c4171c0525154d746e37395e0
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-08 16:43:03 +0000

    first set of files for task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: string to convert
+ *
+ * Return: converted number, or 0 if not binary or B is Null
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+
+	for (; *b; b++)
+	{
+		if (*b == '1')
+			i = (i << 1) | 1;
+		else if (*b == '0')
+			i <<= 1;
+		else
+			return (0);
+	}
+
+	return (i);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+# about
+## bit manipulation
