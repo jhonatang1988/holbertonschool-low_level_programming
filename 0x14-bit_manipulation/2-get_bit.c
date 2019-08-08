885a8e73e2b8834e557d947063379d672429aff6
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-08 18:42:12 +0000

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int res;
 
+	if (index >= 64)
+		return (-1);
+
 	res = ((n >> index) & 1) ? 1 : 0;
 	if (res == 1 || res == 0)
 		return (res);
