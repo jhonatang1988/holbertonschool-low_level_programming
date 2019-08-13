78b3ccaedf427e4d5a547d4b07c8df2a6db7e570
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-13 22:35:08 +0000

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,10 +18,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
-	if (!av[1])
-		error_98("(nil)");
-	if(!av[2])
-		error_99("(nil)");
 	fd = open(av[1], O_RDWR);
 	if (fd == -1)
 		error_98(av[1]);
