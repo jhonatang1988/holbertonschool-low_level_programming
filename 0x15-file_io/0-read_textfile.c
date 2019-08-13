7951ab868d87a5758dfd9f017d13e391089d4d5b
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-13 19:09:24 +0000

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,15 +13,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf = NULL;
 	int r, w;
 
+	if (!filename)
+		return (0);
+
 	buf = (char *)malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
 
-	if (!filename)
-		return (0);
-
 	/* read */
-	fd = open("Requiescat", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
