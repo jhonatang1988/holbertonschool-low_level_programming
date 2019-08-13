5825f8a2f0a9bd99ff8562b5cdc50ae9ff6fc6ad
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-13 18:55:39 +0000

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,10 +11,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buf = NULL;
-	int r;
-	unsigned int len;
+	unsigned int len, r, w;
 
 	buf = (char *)malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
 
 	if(!filename)
 		return (0);
@@ -26,15 +27,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	r = read(fd, buf, letters);
-
+	if (r < 1)
+		return (0);
 	len = strlen(buf);
 
 	if (len < letters)
 		letters = len;
 
-	write(STDOUT_FILENO, buf, letters);
+	w = write(STDOUT_FILENO, buf, letters);
+	if (w < 1)
+		return (0);
 
 	close(fd);
+	free(buf);
 
-	return (r);
+	return (letters);
 }
