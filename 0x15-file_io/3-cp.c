c3807f36d4813a24d3571ee3e344492aea5f52a8
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-13 23:03:01 +0000

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
