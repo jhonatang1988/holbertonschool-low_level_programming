090a5619fef0470935d8fb571f4bff53dd48327d
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-09-14 20:05:07 +0000

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = NULL;
 	unsigned int i = 0;
 
-	if (!h || !*h)
+	if (!h)
 		return (NULL);
 
 	temp = *h;
