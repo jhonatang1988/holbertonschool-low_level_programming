97422541a8ab01fbe73ca8ce87fdad10ceabde89
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-10-30 14:30:51 +0000

    2

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -8,29 +8,28 @@
  *
  * Return: nothing
  */
-
 void selection_sort(int *array, size_t size)
 {
-        int temp = 0, min = 0, pos;
-        size_t i = 0, j = 0;
+	int temp = 0, min = 0, pos;
+	size_t i = 0, j = 0;
 
-        for(i = 0; i < size; i++)
-        {
-                min = array[i];
-                for(j = i; j < size; j++)
-                {
-                        if(min > array[j])
-                        {
-                                min = array[j];
-                                pos = j;
-                        }
-                }
-                if(min != array[i])
-                {
-                        temp = array[i];
-                        array[i] = min;
-                        array[pos] = temp;
-                        print_array(array, size);
-                }
-        }
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				pos = j;
+			}
+		}
+		if (min != array[i])
+		{
+			temp = array[i];
+			array[i] = min;
+			array[pos] = temp;
+			print_array(array, size);
+		}
+	}
 }
