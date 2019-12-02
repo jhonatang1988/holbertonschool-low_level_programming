fd9f20b9687a0d9b57c4db57dd6fd76109a951bb
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-12-02 19:41:40 +0000

    2

---
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts a node at the right side
+ *
+ * @parent: parent node
+ * @value: data
+ *
+ * Return: pointer to the new node or NULL if failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tempNode = parent;
+	binary_tree_t *newNode = NULL;
+	binary_tree_t *secondLevel = NULL;
+
+	// si parent es NULL por requerimiento
+	if (!tempNode)
+		return NULL;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (!newNode)
+		return NULL;
+
+	newNode->parent = tempNode;
+	newNode->n = value;
+	newNode->left = NULL;
+
+	// el parent no tiene right
+	if(!tempNode->right)
+	{
+		tempNode->right = newNode;
+		newNode->right = NULL;
+	}
+	else
+	{
+		secondLevel = tempNode->right;
+		tempNode->right = newNode;
+		newNode->right = secondLevel;
+		secondLevel->parent = newNode;
+	}
+	return newNode;
+}
