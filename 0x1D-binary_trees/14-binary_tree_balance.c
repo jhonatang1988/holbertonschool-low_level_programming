9e9cc1f883227854f11fd054145dd5549b6f8689
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-12-03 20:59:53 +0000

    14

---
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,62 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor
+ *
+ * @tree: pointer to the node
+ *
+ * Return: balance factor or 0 otherwise
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	const binary_tree_t *tempNode = tree;
+	int balanceFactor = 0;
+	int parteLeft = 1;
+
+	if(!tempNode)
+		return balanceFactor;
+
+	while(1)
+	{
+		if(tempNode->left)
+			parteLeft = 1;
+		else
+			parteLeft = 0;
+		//parte left
+		if(parteLeft)
+		{
+			if(tempNode->left)
+			{
+				tempNode = tempNode->left;
+				balanceFactor++;
+			}
+			if(tempNode->right)
+			{
+				tempNode = tempNode->right;
+				balanceFactor++;
+			}
+			else
+			{
+				parteLeft = 0;
+			}
+		}
+		//parte right
+		else
+		{
+			if(tempNode->left)
+			{
+				tempNode = tempNode->left;
+				balanceFactor--;
+			}
+			if(tempNode->right)
+			{
+				tempNode = tempNode->right;
+				balanceFactor--;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return balanceFactor;
+}
