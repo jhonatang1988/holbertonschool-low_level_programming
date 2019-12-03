1329d16787bad863bf1d5d43470825c8f7249615
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-12-03 18:44:33 +0000

    11

---
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - measures the size of binary tree
+ *
+ * @tree: pointer to the node
+ *
+ * Return: the size or 0 in errors
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	const binary_tree_t *tempNode = tree;
+
+	if(!tempNode)
+	{
+		return 0;
+	}
+	else
+	{
+		return(binary_tree_size(tempNode->left) + 1 +
+		       binary_tree_size(tempNode->right));
+	}
+}
